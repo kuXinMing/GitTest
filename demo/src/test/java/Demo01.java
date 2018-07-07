e6c2ff2567069f126a2275e35ec06ea736c8f48a
lc_wanghao <ssrs_wanghao@139.com@10.73.158.222>
2018-07-07 15:05:58 +0800

    添加项目 Python 母版

---
+++ demo/src/test/java/Demo01.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * 第一个测试类
+ *
+ * @author wanghao
+ * @create 2018-07-07 下午3:04
+ **/
+public class Demo01 {
+
+    @Test
+    public void test(){
+        System.out.println("Hello Word");
+    }
+}
